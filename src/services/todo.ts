fd3c30509dcd8d9d3c6e0d8c0b808fa6c8c2be6a
Uncoded_Mind <leonheydemann@gmail.com>
2025-09-17 13:57:37 +0200

    add todos to localStorage

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -4,40 +4,55 @@ export type TodoItem = {
     description: string;
     date: string;
 };
-
+export const STORAGE_KEY = 'ryzeTodo_todos';
 
 export class TodoService {
     private todos: TodoItem[] = [];
 
+    constructor() {
+        this.loadTodos();
+    }
+
+    private loadTodos() {
+        const storedTodos = localStorage.getItem(STORAGE_KEY);
+        if (storedTodos) {
+            this.todos = JSON.parse(storedTodos);
+        }
+    }
+
+    private saveTodos() {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+    }
 
     getAllTodos(): TodoItem[] {
         return [...this.todos];
     }
 
-
     addTodo(todo: Omit<TodoItem, 'id'>): TodoItem {
-        //first entry will always have the highest id
-        const nextTodoId = this.todos.length === 0 ? 1 : this.todos[0].id + 1
+        const nextTodoId = this.todos.length === 0
+            ? 1
+            : Math.max(...this.todos.map(todo => todo.id)) + 1;
         const newItem: TodoItem = { ...todo, id: nextTodoId };
         this.todos.push(newItem);
+        this.saveTodos();
         return newItem;
-
     }
-
 
     updateTodos(id: number, update: Omit<TodoItem, 'id'>): TodoItem | null {
         const idx = this.todos.findIndex(todo => todo.id === id);
         if (idx === -1) return null;
         this.todos[idx] = { ...update, id };
+        this.saveTodos();
         return this.todos[idx];
     }
-
 
     findTodo(id: number) {
         return this.todos.find(todo => todo.id === id);
     }
 
     removeTodo(id: number) {
-        return this.todos.filter((todo) => todo.id !== id)
+        this.todos = this.todos.filter(todo => todo.id !== id);
+        this.saveTodos();
+        return this.todos;
     }
 }
